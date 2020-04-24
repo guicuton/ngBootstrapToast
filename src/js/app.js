46ba71e0953d143a403399124687c833db373aff
Guilherme Mascarenhas <37594431+guicuton@users.noreply.github.com>
2020-04-24 02:13:22 -0300

    bug fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,14 @@ app.controller('mainCtrl', ['$scope', function(s){
     s.toasters = [
         {
             delay: 10000,
+            id: 1234,
             avatar: 'my/image/url.jpg',
             title: 'My first title',
             message: 'My first message with details'
         },
         {
             delay: 10000,
+            id: 1234,
             avatar: 'my/image/url.jpg',
             title: 'My second title',
             message: 'My second message with details'
